3079d91d5cd14a903c23315f5f5e6944fb7c9469
NaveenKarippai <naveenranjankarippai@gmail.com>
2015-08-08 00:23:23 +0200

    second  commit - dev comments

--- ImageDownloader.py
+++ ImageDownloader.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python
 
 '''
-Created on 07/08/2015
+Created on 08/08/2015
 
 @author: karippain
 
 '''
 import urllib,os
 
-"""relative path"""
+"""relative file path"""
+dir = os.path.dirname(__file__)						
+ImageBook = os.path.join(dir, 'ImageCollection.txt')		#relative path text file 
 
-dir = os.path.dirname(__file__)		
-print "this is where we are:",dir				
-ImageBook = os.path.join(dir, 'ImageCollection.txt')
 
 """read text file"""
-
 with open(ImageBook) as textFile:
-    textFileContent = textFile.readlines()
+    textFileContent = textFile.readlines()					#text file data read 
+
 
 """download images"""
-
 for idx,val in enumerate(textFileContent):
-    urllib.urlretrieve(val, dir+"/"+str(idx)+".jpg")
+    urllib.urlretrieve(val, dir+"/"+str(idx)+".jpg")		#write url
     
     
  
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Image-Downloader
-Python script to read image urls from .txt file , download and store images on local drive 
+Python script to read urls from .txt file , download and store url data (images) on local drive 
+
+
+#Information
+python modules included: urllib,os
+
+1. Add url of images sperated by newline chartacter on ImageCollection.text file 
+2. Run ImageDownloader.py Python script
+3. Images will be downloaded on same directory level 
+
