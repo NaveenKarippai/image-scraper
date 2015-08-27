6e3de248da106c2ccf05c0211d45074949526c09
NaveenKarippai <naveenranjankarippai@gmail.com>
2015-08-27 17:26:38 +0200

    PEP 8 test pass

--- ImageDownloader.py
+++ ImageDownloader.py
@@ -6,22 +6,23 @@ Created on 08/08/2015
 @author: karippain
 
 '''
-import urllib,os
 
-"""relative file path"""
+import urllib
+import os
+
+# relative file path
 dir = os.path.dirname(__file__)						
-ImageBook = os.path.join(dir, 'ImageCollection.txt')		#relative path text file 
+ImageBook = os.path.join(dir, 'ImageCollection.txt')		
 
 
-"""read text file"""
+# read text file
 with open(ImageBook) as textFile:
-    textFileContent = textFile.readlines()					#text file data read 
+    textFileContent = textFile.readlines()					
 
 
-"""download images"""
-for idx,val in enumerate(textFileContent):
-    urllib.urlretrieve(val, dir+"/"+str(idx)+".jpg")		#write url
-    
+# download images
+for idx, val in enumerate(textFileContent):
+    urllib.urlretrieve(val, dir+"/"+str(idx)+".jpg")		    
     
  
 
