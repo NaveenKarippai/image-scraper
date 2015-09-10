8d0c0ba63b5acc2075c4a110cc40034d9fe312e6
NaveenKarippai <naveenranjankarippai@gmail.com>
2015-09-10 22:01:28 +0200

    class oriented code

--- ImageDownloader.py
+++ ImageDownloader.py
@@ -1,37 +1,44 @@
 #!/usr/bin/python
 
 '''
-Created on 08/08/2015
+created on 08/08/2015
 
-@author: karippain
+@author: Naveen Karippai
 '''
 
 import urllib
 import os
 
-# relative file path
-def filePath(fileName):
-	dirPath = os.path.dirname(__file__)						
-	relFilePath = os.path.join(dirPath, fileName)
-	return dirPath, relFilePath		
+class ImagesDownload(object):
+		
+	# relative file path
+	def filePath(self, fileName):
+		dirPath = os.path.dirname(__file__)						
+		relFilePath = os.path.join(dirPath, fileName)
+		return dirPath, relFilePath		
 
-# read file content
-def readFile(filePath):
-	with open(filePath) as textFile:
-	    textFileContent = textFile.readlines()
-    	return textFileContent	
+	# read file content
+	def readFile(self, filePath):
+		with open(filePath) as textFile:
+		    textFileContent = textFile.readlines()
+	    	return textFileContent	
 
-# download images				
-def downloadImages(dirname, urlData):
-	for idx, val in enumerate(urlData):
-	    urllib.urlretrieve(val, dirname+"/"+str(idx)+".jpg")		    
+	# download images				
+	def downloadImages(self, dirname, urlData):
+		for idx, val in enumerate(urlData):
+			urllib.urlretrieve(val, dirname+"/"+str(idx)+".jpg")		    
 
-try:
-	dirname, imageBookPath = filePath('ImageCollection.txt')
-	imageBookData = readFile(imageBookPath)
-	downloadImages(dirname, imageBookData)    
-except:
- 	print "Error: unable to download"
+if __name__ == '__main__':
+
+	try:
+		images = ImagesDownload()
+		dirname, imageBookPath = images.filePath('ImageCollection.txt')
+		imageBookData = images.readFile(imageBookPath)
+		images.downloadImages(dirname, imageBookData)
+
+	except:
+		print "Error: unable to download"
+
 
 
 
