d4b5c40f1a8087bd0bd7c1dfbac2634218d9c822
Nathan Abu <nathanabu@gmail.com>
2015-09-14 04:08:45 +0300

    multiprocessing and some refactoring
    Use mutliprocessing module in order to parallel the images download.
    Use original image name instead of numbering so it will be easier from CLI to know what we actually got.
    Set download timeout (actually process activity timeout)
    Refactor 'parseImgLinks'
    In "__main__" sections, since we are not doing anything else except from calling our methods, better show the exception message and type if it appears, so no need to catch it, we will exit anyway.

--- ImageDownloader.py
+++ ImageDownloader.py
@@ -2,11 +2,16 @@
 
 import urllib
 import os
+from urlparse import urlparse
+from os.path import splitext,basename
+from multiprocessing import Pool,TimeoutError
 
 class ImagesDownload(object):
 
-	def __init__(self, fileName = None):
+	def __init__(self, pool_size=5,timeout=10,fileName = None):
 		self.fileName = fileName
+		self._pool_size = pool_size
+		self._timeout = timeout
 		
 	# relative file path
 	def filePath(self):
@@ -20,26 +25,41 @@ class ImagesDownload(object):
 	# read file content
 	def readFile(self, filePath):
 		with open(filePath) as textFile:
-		    textFileContent = textFile.readlines()
-	    	return textFileContent	
+			textFileContent = textFile.readlines()
+			return textFileContent
 
 	# download images				
 	def downloadImages(self, dirName, urlData):
-		if not os.path.exists('pictures'): os.makedirs('pictures')
-		dirName = dirName+'/pictures'
-		for idx, val in enumerate(urlData):
-			urllib.urlretrieve(val, dirName+"/"+str(idx)+".jpg")		    
+		child_folder = 'pictures'
+		# EAFP (see https://docs.python.org/2/glossary.html)
+		try:
+			os.makedirs(child_folder)
+		except:
+			pass
+		dirName = os.path.join(dirName,child_folder)
+		# We can use a process pool to do it in parallel
+		process_pool = Pool(processes=self._pool_size)
+		results = []
+
+		for ud in urlData:
+			# Lets use source images names instead of numbering
+			filename = ''.join(splitext(basename(urlparse(ud).path)))
+			results.append( process_pool.apply_async( urllib.urlretrieve, [ ud,  os.path.join(dirName,filename) ] ) )
+
+		for result in results:
+			try:
+				result.get(self._timeout)
+			except TimeoutError:
+				print("Could not download image due to timeout")
+			except Exception as e:
+				print(e)
+
 
 if __name__ == '__main__':
-
-	try:
 		images = ImagesDownload('ImageCollection.txt')
 		dirName, imageBookPath = images.filePath()
 		imageBookData = images.readFile(imageBookPath)
 		images.downloadImages(dirName, imageBookData)
-
-	except:
-		print "Error: unable to download"
 
 
 
--- ImageScraper.py
+++ ImageScraper.py
@@ -2,36 +2,29 @@
 
 from BeautifulSoup import BeautifulSoup
 import urllib2
+import urlparse
 import re
 import ImageDownloader
 
 class ImageScrapy(ImageDownloader.ImagesDownload):
 
-	def __init__(self, scrap_link):
-		ImageDownloader.ImagesDownload.__init__(self)
+	def __init__(self, scrap_link, *args,**kwargs):
+		super(ImageScrapy,self).__init__(*args,**kwargs)
 		self.scrap_url = scrap_link
-		self.img_list = []
+		self.img_list = set() # Use set to avoid duplicates which we might have
 
 	# extract all img src from webpage
 	def parseImgLinks(self):
 		url_response = urllib2.urlopen(self.scrap_url)
 		html_parse = BeautifulSoup(url_response)
-		img_elements = html_parse.findAll('img')
-		for element in img_elements:
-			img_src = element['src']
-			if not img_src.startswith('http'):
-				img_src = '%s%s' % ('http:', img_src)
-			urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', img_src)
-			self.img_list.extend(urls)
+
+		for src in [ e['src'] for e in html_parse.findAll('img') if not e['src'].startswith('//') ]:
+			abs_url = src if urlparse.urlparse(src).netloc else urlparse.urljoin(self.scrap_url,src)
+			self.img_list.add(abs_url)
 		return self.img_list
 
 if __name__ == '__main__':
-
-	try:
 		scrap_images = ImageScrapy('http://www.people.com/people/jennifer_lawrence/')
 		dirName = scrap_images.filePath()
 		img_list = scrap_images.parseImgLinks()
 		scrap_images.downloadImages(dirName, img_list)
-
-	except:
-		print "Error: unable to download"
