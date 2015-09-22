75d521b862feb14dc2831e1d7f99258bdaf337d5
NaveenKarippai <naveenranjankarippai@gmail.com>
2015-09-22 20:02:12 +0200

    added test method and image download bug fix

--- tests/test_ImageDownloader.py
+++ tests/test_ImageDownloader.py
@@ -14,6 +14,7 @@ class TestImageDownloader(unittest.TestCase, ImageDownloader.ImagesDownload):
 
 	def setUp(self):
 		self.images = ImageDownloader.ImagesDownload(fileName = None)
+		self.mock_data = ['a.png', 'b.jpg', 'c.jpg']
 
 	def test_instance(self):
 		self.assertIsInstance(self.images, ImageDownloader.ImagesDownload, 'class ImagesDownload instantiation fail')   
@@ -21,6 +22,10 @@ class TestImageDownloader(unittest.TestCase, ImageDownloader.ImagesDownload):
 	def test_filePath(self):
 		path = self.images.filePath()
 		self.assertIsInstance(path, str, 'class ImagesDownload method filePath fail')
+
+	def test_readFile(self):
+		fileData = self.images.readFile(self.images.filePath()+'/tests/mock_test/mock_image_directory.txt')
+		self.assertItemsEqual(fileData, self.mock_data,'class ImagesDownload method readFile fail')
 
 	def tearDown(self):
 		pass
